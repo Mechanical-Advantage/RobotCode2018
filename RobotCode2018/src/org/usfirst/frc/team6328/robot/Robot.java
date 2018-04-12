7ddd5671863d88d1733206bc150b065d947e9535
e-bonner <e-bonner@users.noreply.github.com>
2018-04-12 10:02:40 -0400

    Remove pneumatics test

--- RobotCode2018/src/org/usfirst/frc/team6328/robot/Robot.java
+++ RobotCode2018/src/org/usfirst/frc/team6328/robot/Robot.java
@@ -21,7 +21,6 @@ import org.usfirst.frc.team6328.robot.commands.DriveDistanceOnHeading;
 import org.usfirst.frc.team6328.robot.commands.DriveWithJoystick.JoystickMode;
 import org.usfirst.frc.team6328.robot.commands.EnableAllElevatorSlaves;
 import org.usfirst.frc.team6328.robot.commands.GenerateMotionProfiles;
-import org.usfirst.frc.team6328.robot.commands.PneumaticsTest;
 import org.usfirst.frc.team6328.robot.commands.RunMotionProfileOnRio;
 import org.usfirst.frc.team6328.robot.commands.SmartSideAuto;
 import org.usfirst.frc.team6328.robot.commands.SwitchStraightDelivery;
@@ -169,9 +168,6 @@ public class Robot extends TimedRobot {
 			@SuppressWarnings("unused")
 			Compressor c = new Compressor();
 		}
-		
-		Command test = new PneumaticsTest();
-		SmartDashboard.putData(test);
 	}
 	
 	@Override
